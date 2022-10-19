de341c9492f543a6dca8f06a349ee37e8bb24172
Raphael Malims <raphaelmalimsj@gmail.com>
2022-10-20 00:53:49 +0300

    Upload on the decompressed traffic toll data.

--- ETL_toll_data.py
+++ ETL_toll_data.py
@@ -18,60 +18,46 @@ default_args = {
 dag = DAG(
     dag_id='ETL_toll_data',
     default_args=default_args,
-    description='Apache Airflow Final Assignment',
+    description='Apache Airflow Traffic Data',
     schedule_interval=timedelta(days=1),
 )
 
 #Define the tasks
-#Task download data.
-download_data = BashOperator(
-	task_id='download_data',
-	bash_command='wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Final Assignment/tolldata.tgz',
-	dag=dag,
-)
-
 #define the first task names unzip_data
 unzip_data = BashOperator(
     task_id='unzip_data',
-    bash_command = 'tar -zxvf tolldata.tgz -C /home/project/airflow/dags/finalassignment',
+    bash_command = 'tar -zxvf  ~/Zen/Airflow-ETL_Toll_Data/tolldata.tgz',
     dag=dag,
 )
 
 #Task to extract data from csv file
 extract_data_from_csv = BashOperator(
     task_id = 'extract_data_from_csv',
-    bash_command = 'cut -d"," -f1,2,3,4 /home/project/airflow/dags/finalassignment/vehicle-data.csv > /home/project/airflow/dags/finalassignment/csv_data.csv',
+    bash_command = 'cut -d"," -f1,2,3,4 ~/Zen/Airflow-ETL_Toll_Data/vehicle-data.csv',
     dag=dag,
 )
 
 #Task to extract data from tsv file.
 extract_data_from_tsv = BashOperator(
     task_id='extract_data_from_tsv',
-    bash_command='cut -d$"\t" -f5,6,7 /home/project/airflow/dags/finalassignment/tollplaza-data.tsv > /home/project/airflow/dags/finalassignment/tsc_data.csv',
+    bash_command='cut -d$"\t" -f5,6,7 ~/Zen/Airflow-ETL_Toll_Data/tollplaza-data.tsv',
     dag=dag,
 )
 
 #Task to extract data from fixed width file.
 extract_data_from_fixed_width = BashOperator(
     task_id='extract_data_from_fixed_width',
-    bash_command='cut -d" "-f6-7 /home/project/airflow/dags/finalassignment/paymeny-data.txt > /home/project/airflow/dags/finalassignment/fixed_width_data.csv',
+    bash_command='cut -d " " -f 6-7 ~/Zen/Airflow-ETL_Toll_Data/payment-data.txt',
     dag=dag,
 )
 
 #Task to consolidate data
 consolidate_data=BashOperator(
     task_id='consolidate_data',
-    bash_command='paste csv_data.csv tsv_data.csv fixed_width_data.csv > extracted_data.csv',
+    bash_command='paste vehicle-data.csv > tollplaza-data.tsv > payment-data.txt',
     dag=dag,
 )
 
-#Task to transform data
-transform_data=BashOperator(
-    task_id='transform_data',
-    bash_command='awk -F "," '{print $1","$2","$3","toupper($4)","$5","$6","$7","$8","$9}'  < extracted_data.csv > /home/project/airflow/dags/finalassignment/staging_transformed_data.csv',
-    dag=dag,
-)
 
 #Task pipeline
-download_data >> unzip_data >> extract_data_from_csv >> extract_data_from_tsv >> extract_data_from_fixed_width >> consolidate_data >> transform_data
-
+unzip_data >> extract_data_from_csv >> extract_data_from_tsv >> extract_data_from_fixed_width >> consolidate_data 
